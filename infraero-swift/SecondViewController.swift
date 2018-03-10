a8cd0bd39a7c76389a2c04ec239d5bb0b7e1a9e6
Thiago Durante Pires <thdurante@gmail.com>
2018-03-10 10:07:21 -0300

    Initial Commit

---
+++ infraero-swift/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  infraero-swift
+//
+//  Created by Thiago Durante Pires on 10/03/2018.
+//  Copyright © 2018 CodingLab. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
